7350590a3a2d9161f9c04191ff701e97cb69666d
ntshuxeko16lll-star <ntshu.xeko16lll@gmail.com>
2026-04-09 17:11:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 
+const SUPABASE_URL = "https://llooewepqlkcpqzmiuzo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vYhWHzf0GkDxch6hp9QmAA_kXkJEu6C";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

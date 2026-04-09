6f1e39188a82644c2553a2b309f7f23221678d62
ntshuxeko16lll-star <ntshu.xeko16lll@gmail.com>
2026-04-09 17:12:30 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,39 @@
+const loginBtn = document.getElementById("login-btn");
+const signupBtn = document.getElementById("signup-btn");
 
+loginBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    showFeed();
+  }
+});
+
+signupBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    alert("Account created");
+  }
+});
+
+function showFeed() {
+  document.getElementById("auth-screen").classList.remove("active");
+  document.getElementById("feed-screen").classList.add("active");
+    }

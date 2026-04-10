39e3cb4fc50b7c684b50e82a08d3bae05a854ea8
ntshuxeko16lll-star <ntshu.xeko16lll@gmail.com>
2026-04-10 11:28:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,1 @@
-
-const SUPABASE_URL = "https://llooewepqlkcpqzmiuzo.supabase.co";
-const SUPABASE_KEY = "sb_publishable_vYhWHzf0GkDxch6hp9QmAA_kXkJEu6C";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+console.log("App loaded");

e6401b5c31c093a2faa26867f2a9f1073279ec2b
ntshuxeko16lll-star <ntshu.xeko16lll@gmail.com>
2026-04-10 11:37:00 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("App loaded");
+const SUPABASE_URL = "https://llooewepqlkcpqzmiuzo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vYhWHzf0GkDxch6hp9QmAA_kXkJEu6C";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// CHECK USER
+async function checkUser() {
+  const { data } = await supabase.auth.getUser();
+
+  if (!data.user) {
+    window.location.href = "index.html";
+  }
+}
+
+checkUser();

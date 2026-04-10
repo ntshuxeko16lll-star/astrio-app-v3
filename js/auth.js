32249d5c74c532435a58a165cbb2e2c59229fb0c
ntshuxeko16lll-star <ntshu.xeko16lll@gmail.com>
2026-04-10 11:29:23 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,7 @@
+document.getElementById("login-btn").onclick = () => {
+  window.location.href = "app.html";
+};
 
+document.getElementById("signup-btn").onclick = () => {
+  alert("Signup next step");
+};

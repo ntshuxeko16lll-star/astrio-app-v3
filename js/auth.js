4f32450e83b4da49e6f01895885e606083cb04d0
ntshuxeko16lll-star <ntshu.xeko16lll@gmail.com>
2026-04-10 11:35:59 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,41 @@
-document.getElementById("login-btn").onclick = () => {
-  window.location.href = "app.html";
+const SUPABASE_URL = "https://llooewepqlkcpqzmiuzo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vYhWHzf0GkDxch6hp9QmAA_kXkJEu6C";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const loginBtn = document.getElementById("login-btn");
+const signupBtn = document.getElementById("signup-btn");
+
+// LOGIN
+loginBtn.onclick = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    window.location.href = "app.html";
+  }
 };
 
-document.getElementById("signup-btn").onclick = () => {
-  alert("Signup next step");
+// SIGN UP
+signupBtn.onclick = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    alert("Account created. Check your email.");
+  }
 };
